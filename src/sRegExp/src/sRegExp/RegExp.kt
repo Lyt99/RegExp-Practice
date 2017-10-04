819aa140ef0b826c8b01b60f55f1ba5c3c3eb3c5
Lyt99 <Lyt99>
2017-10-05 01:06:30 +0800

    添加非编译模式

--- src/sRegExp/src/sRegExp/RegExp.kt
+++ src/sRegExp/src/sRegExp/RegExp.kt
@@ -15,6 +15,31 @@ object RegExp{
         var r = CompiledRegExp(regexp)
         return r.findAll(text)
     }
+
+    fun replace(regexp : String, text:String, rep : String) : String {
+        var r = CompiledRegExp(regexp)
+        return r.replace(text, rep)
+    }
+
+    fun replace(regexp : String, text : String, func : (r : String) -> String) : String {
+        var r = CompiledRegExp(regexp)
+        return r.replace(text, func)
+    }
+
+    fun replaceOne(regexp : String, text : String, rep : String) : String {
+        var r = CompiledRegExp(regexp)
+        return r.replace(text, rep)
+    }
+
+    fun replaceOne(regexp : String, text : String, func : (r : String) -> String) : String {
+        var r = CompiledRegExp(regexp)
+        return r.replaceOne(text, func)
+    }
+
+    fun split(regexp : String, text : String) : Array<String> {
+        var r = CompiledRegExp(regexp)
+        return r.split(text)
+    }
 }
 
 
